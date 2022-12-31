373f5fe0a7b702766bc4bea6835a56ffc34d507b
Oguzhan <ogigogi56@gmail.com>
2022-12-31 19:44:14 +0300

    Initial Commit

---
+++ ökmönö/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ökmönö
+//
+//  Created by oğuzhan salkım on 31.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
